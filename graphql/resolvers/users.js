e20e863b8be4620bf31a001f7238178863982d14
Abhishek Verma <zabhishek.verma@gmail.com>
2022-03-13 15:24:29 +0530

    Register & Login Users: Make sure user doesnt already exist

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { UserInputError } = require("apollo-server");
 
 const { SECRET_KEY } = require("../../config");
 const User = require("../../models/User");
@@ -8,11 +9,16 @@ module.exports = {
   Mutation: {
     async register(
       _,
-      { registerInput: { username, email, password, confirmPassword } },
-      context,
-      info
+      { registerInput: { username, email, password, confirmPassword } }
     ) {
-      // hash password and create an authnetication token
+      const user = await User.findOne({ username });
+      if (user) {
+        throw new UserInputError("Username is taken", {
+          errors: {
+            username: "this username is taken",
+          },
+        });
+      }
       password = await bcrypt.hash(password, 12);
       const newUser = new User({
         email,

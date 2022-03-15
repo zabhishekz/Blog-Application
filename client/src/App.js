67a8eea77d6649adefe689431253dfec6ec43114
Abhishek Verma <zabhishek.verma@gmail.com>
2022-03-15 23:54:42 +0530

    Semantic UI & Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Container } from "semantic-ui-react";
+
+import "semantic-ui-css/semantic.min.css";
 import "./App.css";
+
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import MenuBar from "./componenets/MenuBar";
 
 function App() {
   return (
-    <div>
-      <h1>Hi world!</h1>
-    </div>
+    <Router>
+      <Container>
+        <MenuBar />
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+        </Routes>
+      </Container>
+    </Router>
   );
 }
 

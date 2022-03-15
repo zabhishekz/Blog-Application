f6a47f39dea227f48d073fcd9f9edb57ff8ea2e8
Abhishek Verma <zabhishek.verma@gmail.com>
2022-03-15 20:41:27 +0530

    Client Start & Apollo Setup

---
+++ client/src/ApolloProvider.js
@@ -0,0 +1,23 @@
+import React from "react";
+import App from "./App";
+import {
+  ApolloClient,
+  InMemoryCache,
+  HttpLink,
+  ApolloProvider,
+} from "@apollo/client";
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:5000",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+export default (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);

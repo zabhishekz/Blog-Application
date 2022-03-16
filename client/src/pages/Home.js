9559a64bddb220b0224d6076ce78d19533c7751d
Abhishek Verma <zabhishek.verma@gmail.com>
2022-03-16 23:03:52 +0530

    Loin & register Pages

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -6,10 +6,8 @@ import { Grid } from "semantic-ui-react";
 import PostCard from "../components/PostCard";
 
 function Home() {
-  const {
-    loading,
-    data: { getPosts: posts },
-  } = useQuery(FETCH_POSTS_QUERY);
+  const { loading, data: { getPosts: posts } = {} } =
+    useQuery(FETCH_POSTS_QUERY);
 
   return (
     <Grid columns={3}>

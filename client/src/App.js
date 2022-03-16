9b33a6ae99069b6fc6ff6ac6882635ba40f855a7
Abhishek Verma <zabhishek.verma@gmail.com>
2022-03-17 00:21:21 +0530

    Authentication Context

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,9 @@ import { Container } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import "./App.css";
 
+import { AuthProvider } from "./context/auth";
+import AuthRoute from "./util/AuthRoute";
+
 import Home from "./pages/Home";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
@@ -12,16 +15,16 @@ import MenuBar from "./components/MenuBar";
 
 function App() {
   return (
-    <Router>
-      <Container>
-        <MenuBar />
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/register" element={<Register />} />
-        </Routes>
-      </Container>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Container>
+          <MenuBar />
+          <Route exact path="/" component={Home} />
+          <AuthRoute exact path="/login" component={Login} />
+          <AuthRoute exact path="/register" component={Register} />
+        </Container>
+      </Router>
+    </AuthProvider>
   );
 }
 

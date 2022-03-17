2737aa021f229819eeaa7cff04454068975092c3
Abhishek Verma <zabhishek.verma@gmail.com>
2022-03-17 22:47:11 +0530

    Adding Posts

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -6,13 +6,23 @@ import {
   HttpLink,
   ApolloProvider,
 } from "@apollo/client";
+import { setContext } from "apollo-link-context";
 
 const httpLink = new HttpLink({
   uri: "http://localhost:5000",
 });
 
+const authLink = setContext(() => {
+  const token = localStorage.getItem("jwtToken");
+  return {
+    headers: {
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 
 import "semantic-ui-css/semantic.min.css";

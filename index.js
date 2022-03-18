7bedc81cb292da1a91a049702c011ed24df1553a
Abhishek Verma <zabhishek.verma@gmail.com>
2022-03-18 18:55:08 +0530

    package.json changes

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const typeDefs = require("./graphql/typeDefs");
 const resolvers = require("./graphql/resolvers");
 const { MONGODB } = require("./config.js");
 
+const PORT = process.env.PORT || 5000;
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -15,8 +17,11 @@ mongoose
   .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
     console.log("MongoDB connected");
-    return server.listen({ port: 5000 });
+    return server.listen({ port: PORT });
   })
   .then((res) => {
     console.log(`Server running on ${res.url}`);
+  })
+  .catch((err) => {
+    console.error(err);
   });

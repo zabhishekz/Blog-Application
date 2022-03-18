21d822f8577a3130c390b4e0c72554c7344839f8
Abhishek Verma <zabhishek.verma@gmail.com>
2022-03-18 19:59:30 +0530

    Edited proxy and server base uri on client side

--- client/src/ApolloProvider.js
+++ client/src/ApolloProvider.js
@@ -9,7 +9,7 @@ import {
 import { setContext } from "apollo-link-context";
 
 const httpLink = new HttpLink({
-  uri: "http://localhost:5000",
+  uri: "https://social-media-app-289.herokuapp.com/",
 });
 
 const authLink = setContext(() => {
